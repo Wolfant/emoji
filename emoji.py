20856f0e2428ac10ac3cbe9ae335196ef8adcef3
Antonio Insuasti <antonio@insuasti.ec>
2018-12-20 17:03:33 -0500

    Add emoji.py via upload
    add de initial file, just a list of unicode characters

---
+++ emoji.py
@@ -0,0 +1,17 @@
+from __future__ import (unicode_literals, division,
+                        absolute_import, print_function)
+from datetime import datetime
+
+
+fun_emoji = ['🤐', '🤐', '🤑', '🤒', '🤓', '🤔', '🤕', '🤖', '🤗', '🤘', '🤙',
+             '🤚', '🤛', '🤜', '🤝', '🤞', '🤟', '🤠', '🤡', '🤢', '🤣', '🤤',
+             '🤥', '🤧', '🤨', '🤩', '🤪', '🤫', '🤬', '🤭', '🤮', '🤯']
+
+
+def main():
+    for a in fun_emoji:
+        print(a)
+
+
+if __name__ == '__main__':
+    main()
